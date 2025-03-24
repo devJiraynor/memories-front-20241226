58278e904b542cfd6d06ad907312b4b79f2a34e1
devJiraynor <jiraynor@naver.com>
2025-03-24 17:21:39 +0900

    2025.03.24. 17:50

--- src/views/ConcentrationTest/index.tsx
+++ src/views/ConcentrationTest/index.tsx
@@ -9,7 +9,7 @@ import { ResponseDto } from 'src/apis/dto/response';
 import { useNavigate } from 'react-router';
 
 // variable: 전체 시간 (60초) //
-const TOTAL_TIME = 60 * 1000;
+const TOTAL_TIME = 60 * 1000 ;
 // variable: 별 표시 시간 (0.25초) //
 const STAR_TIME = 400;
 // variable: 별 표시 횟수 //
@@ -76,14 +76,15 @@ export default function ConcentrationTest() {
   // effect: 검사 시작 상태가 변경될 시 실행할 함수 //
   useEffect(() => {
     reset();
+    let interval: NodeJS.Timeout;
     if (isStarted) {
       init();
 
       setTimeout(() => {
         setFinish(true);
-      }, TOTAL_TIME);
+      }, TOTAL_TIME + STAR_TIME);
 
-      setInterval(() => {
+      interval = setInterval(() => {
         setStarVisible(true);
         clickRef.current = false;
 
@@ -93,6 +94,10 @@ export default function ConcentrationTest() {
         }, STAR_TIME);
 
       }, Math.floor(TOTAL_TIME / STAR_COUNT));
+    }
+
+    return () => {
+      clearInterval(interval);
     }
   }, [isStarted]);
 
